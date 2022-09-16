a8d58b0160be730defbc22e4b45c87b873b02382
LukeZekes <zechesl@gmail.com>
2022-09-16 16:28:41 -0400

    Update router

--- src/components/DeviceInformation.vue
+++ src/components/DeviceInformation.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div id="device-info">Device information</div>
+    <div id="device-info">
+      {{ device }}
+    </div>
     <div id="device-actions">Device actions</div>
     <div id="energy-info">Energy information</div>
   </div>
@@ -8,9 +10,11 @@
 <script>
 export default {
   name: "DeviceInformation",
-  props: ["device"],
+  props: ["id"],
   data() {
-    return {};
+    return {
+      device: this.id,
+    };
   },
 };
 </script>
