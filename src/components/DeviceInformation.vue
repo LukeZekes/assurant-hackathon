bdce599dd1dff1d832c3914658836cba3b0f6de9
LukeZekes <zechesl@gmail.com>
2022-09-16 10:23:48 -0400

    Initial setup, removing unnecessary files

---
+++ src/components/DeviceInformation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div id="device-info"></div>
+    <div id="device-actions"></div>
+    <div id="energy-info"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "DeviceInformation",
+  props: ["device"],
+  data() {
+    return {};
+  },
+};
+</script>
+<style></style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div id="devices">
+      <DeviceInformation v-for="(device, index) in devices" :key="index" />
+    </div>
+    <div id="energy-info"></div>
+  </div>
+</template>
+<script>
+import DeviceInformation from "./DeviceInformation.vue";
+export default {
+  name: "HomePage",
+  components: {
+    DeviceInformation,
+  },
+  data() {
+    return {
+      devices: [],
+    };
+  },
+};
+</script>
+<style></style>
